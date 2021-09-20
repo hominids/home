1b515ca049a0d1a4e783e2bb791efb453cb22605
tjvsx <vanslotj@gmail.com>
2021-09-20 04:25:32 -0400

    removed unneeded files

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-//import Image from 'next/image'
 import Header from '../components/Header'
 //import Banner from '../components/Banner'
 //import About from '../components/About'
